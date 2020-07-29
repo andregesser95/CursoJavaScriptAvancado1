d9128a87454524381c607ea7e789e6d1520eb879
André Gesser <45247030+andregesser95@users.noreply.github.com>
2020-07-29 09:15:42 -0300

    Inicio curso Avançado II

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,41 +1,44 @@
 class NegociacoesView extends View {
-
+    
     constructor(elemento) {
+        
         super(elemento);
     }
-
+    
     template(model) {
+        
         return `
-            <table class="table table-hover table-bordered">
-                <thead>
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+        
+            <tbody>
+                ${model.negociacoes.map(n => `
+                    
                     <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
                     </tr>
-                </thead>
-
-                <tbody>
-                    ${model.negociacoes.map(n =>
-            `
-                            <tr>
-                                <td>${DateHelper.dataParaTexto(n.data)}</td>
-                                <td>${n.quantidade}</td>
-                                <td>${n.valor}</td>
-                                <td>${n.volume}</td>
-                            </tr>
-                        `
-        ).join('')}
-                </tbody>
-                <tfoot>
-                    <td colspan="3">Total</td>
-                    <td>
-                        ${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
-                    </td>
-                </tfoot>
-            </table>
+                    
+                `).join('')}                
+            </tbody>
+                  
+            <tfoot>
+                <td colspan="3"></td>
+                <td>
+                    ${model.volumeTotal}
+                </td>
+            </tfoot>
+            
+        </table>
         `;
     }
-
 }
